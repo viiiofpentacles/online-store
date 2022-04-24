f618ce4a794ebb376d230a1700a054cb0cc72d6f
bea <chamoischamois@protonmail.com>
2022-04-24 20:24:03 +0800

    Set up pages and routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import './styles/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './Home';
+import Store from './Store';
+import About from './About';
+import Contact from './Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path ="store" element={<Store />} />
+          <Route path ="about" element={<About />} />
+          <Route path ="contact" element={<Contact />} />
+          <Route path ="*" element={
+            <main>
+              <h1>Page Not Found</h1>
+              <p>Can't find what you're looking for? <Link to="/contact">Reach out to us.</Link></p>
+            </main>
+          }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
