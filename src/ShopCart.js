c40b3cd3a8d2c19ed618715240820d00fd276cc1
bea <chamoischamois@protonmail.com>
2022-04-30 18:54:59 +0800

    Added ShopCart tests and ShopCart component

--- src/ShopCart.js
+++ src/ShopCart.js
@@ -1,17 +1,13 @@
-import React, { useState, useEffect } from 'react';
 // goes into App as a footer, only visible with atleast one item in cart
 
-const ShopCart = () => {
-    const [cart, setCart] = useState({
-        qty: 0,
-        total: 0
-    });
-
+const ShopCart = (props) => {
+    const cartTotal = (props.appState.total).toFixed(2);
+    
     return (
         <div className='cart'>
-            <div>{cart.qty} items in cart </div>
-            <div>Cart Total: {cart.total}</div>
-            <button className='checkout-button'>Checkout</button>
+            <div>{props.appState.totalQty} items in cart </div>
+            <div>Cart Total: {cartTotal}</div>
+            <button className='checkout-button' onClick={props.handleCheckout}>Checkout</button>
         </div>
     );
 };
--- src/tests/ShopCart.test.js
+++ src/tests/ShopCart.test.js
@@ -1,6 +1,35 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import ShopCart from "../ShopCart";
 
 describe("Shopping Cart", () => {
-})
+    const mockCheckout = jest.fn();
+    const mockState = {
+        items: [
+            {id: 1,
+             qty: 1
+            }
+        ],
+        totalQty: 1,
+        total: 14.00
+    }
+
+    it('shows item qty when there is 1 item in cart', () => {
+        render(<ShopCart appState = {mockState} />);
+        expect(screen.getByText(/1 items in cart/i)).toBeInTheDocument();
+    });
+
+    it('renders cart total', () => {
+        render(<ShopCart appState = {mockState} />);
+        expect(screen.getByText(/cart total: 14.00/i)).toBeInTheDocument();
+    });   
+
+    it('renders checkout button that fires function once when clicked', () => {
+        render(<ShopCart appState = {mockState} handleCheckout = {mockCheckout} />);
+        const addToCardButton = screen.getByRole('button');
+        userEvent.click(addToCardButton);
+        expect(mockCheckout).toBeCalledTimes(1);
+    });
+});
