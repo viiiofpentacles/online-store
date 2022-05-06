632b830cd30e109d97499814e7eeb20aa83fb247
bea <chamoischamois@protonmail.com>
2022-05-06 22:50:11 +0800

    Implemented fix for add and reduce quantity buttons

--- src/ItemCard.js
+++ src/ItemCard.js
@@ -1,32 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './styles/ItemCard.css';
 
 const ItemCard = (props) => {
 
     let [qty, setQty] = useState(1);
 
-    useEffect(() => {
-        const minusButton = document.querySelector('.minus-button');
-        if (qty <= 1) {
-            minusButton.disabled = true;
+    function reduceButton (e) {
+        const button = e.target;
+        const inputField = document.querySelector(`[class="${e.target.id}"]`);
+        if (inputField.value <= 1) {
+            button.disabled = true;
+            button.classList.add('disabled');
         } else {
-            minusButton.disabled = false;
-        };
-    });
-
-    function reduceButton () {
+            button.disabled = false;
+            button.classList.remove('disabled');
+        }
         setQty(Number(qty - 1));
     }
 
     function increaseButton () {
+        const button = document.querySelector('.minus-button'); 
+        button.disabled = false;
+        button.classList.remove('disabled');
         setQty(Number(qty + 1));
     }
 
     function handleChange (e) {
         setQty(e.target.value);
+        const buttonId = e.target.className[0];
+        const minusButton = document.getElementById(buttonId);
+        if (e.target.value <=1) {
+            minusButton.disabled = true;
+            minusButton.classList.add('disabled');
+        } else {
+            minusButton.disabled = false;
+            minusButton.classList.remove('disabled');
+        }
     }
 
     function addToCartButton (e) {
+        if(qty <= 0) {
+        }
         props.addItemToCart(props.data.cost, props.data.id);
         const addButton = e.target;
             addButton.textContent = 'Added!'
@@ -46,12 +60,13 @@ const ItemCard = (props) => {
                 <div className="qty-input">
                     <label htmlFor="qty-input">Quantity</label>
                     <div className='input-area'>
-                        <button className="minus-button" onClick={reduceButton}>-</button>
-                        <input type="text" value={qty} id={`qty-input ${props.data.id}`} onChange={handleChange} />
+                        <button className="minus-button" id={props.data.id} onClick={reduceButton}>-</button>
+                        <input type="text" value={qty} id="qty-input" className={props.data.id} onChange={handleChange} />
                         <button className="plus-button" onClick={increaseButton}>+</button>
                         </div>
                 </div>
                 <button className="add-to-cart-button" onClick={addToCartButton}>Add to Cart</button>
+                <div className='error-message'></div>
             </div>
         </div>
     );
